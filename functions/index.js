d22f4a5ca273aeec42678335618ed59b1599e47f
DESKTOP-HF3GHF6\sono8 <sono.stream8580@gmail.com>
2018-11-17 22:31:48 +0900

    Add cloud function

---
+++ functions/index.js
@@ -0,0 +1,51 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const gcs = require('@google-cloud/storage')();
+const Readable = require('stream').Readable;
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+  const original = req.query.text;
+  return admin.database().ref('/update').push({ original: original }).then(snapshot => {
+    return res.redirect(303, snapshot.ref.toString());
+  })
+})
+
+exports.generateNotification = functions.database.ref('/blogs/{pushId}')
+  .onUpdate((change, context) => {
+    let val = change.after.val();
+    let bucket = gcs.bucket('gamma-creators.appspot.com');
+
+    let html =
+      `<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta property="og:type" content="https://gamma-creators.firebaseapp.com">
+    <meta property="og:site_name" content="GAMMA Blog">
+    <meta property="og:title" content="${val.title}">
+    <meta property="og:description" content="${val.preview}">
+  </head>
+  <body>
+  </body>
+</html>`;
+
+    let uploadStream
+      = bucket.file('test').createWriteStream({
+        metadata: { contentType: 'text/html' }
+      });
+
+    let readStream = new Readable();
+    readStream.push(text);
+    readStream.push(null);
+    //return admin.database.ref('update').set(true);
+
+    return new Promise((resolve, reject) => {
+      readStream.on('error', reject).pipe(uploadStream)
+        .on('error', reject).on('finish', resolve);
+    })
+  });
